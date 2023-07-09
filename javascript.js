c2a4960d144b5bf88e23a9c785366baa44e4566d
akash kumar <111784316+akashkumar8115@users.noreply.github.com>
2023-07-09 15:07:44 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,93 @@
+var i = 0;
+function showi() {
+    if (i == 0) {
+        document.getElementById("showupi").style.display = "block";
+        document.getElementById("crossi").classList.toggle("rotatedi");
+        i++
+    }
+    else {
+        document.getElementById("showupi").style.display = "none";
+        document.getElementById("crossi").classList.toggle("rotatedi");
+        i--;
+    }
+}
+
+
+var j = 0;
+function showj() {
+    if (j == 0) {
+        document.getElementById("showupj").style.display = "block";
+        document.getElementById("crossj").classList.toggle("rotatedj");
+
+        j++
+    }
+    else {
+        document.getElementById("showupj").style.display = "none";
+        document.getElementById("crossj").classList.toggle("rotatedj");
+
+        j--;
+    }
+}
+
+var k = 0;
+function showk() {
+    if (k == 0) {
+        document.getElementById("showupk").style.display = "block";
+        document.getElementById("crossk").classList.toggle("rotatedk");
+
+        k++
+    }
+    else {
+        document.getElementById("showupk").style.display = "none";
+        document.getElementById("crossk").classList.toggle("rotatedk");
+
+        k--;
+    }
+}
+
+
+var l = 0;
+function showl() {
+    if (l == 0) {
+        document.getElementById("showupl").style.display = "block";
+        document.getElementById("crossl").classList.toggle("rotatedl");
+
+        l++
+    }
+    else {
+        document.getElementById("showupl").style.display = "none";
+        document.getElementById("crossl").classList.toggle("rotatedl");
+
+        l--;
+    }
+}
+
+
+var m = 0;
+function showm() {
+    if (m == 0) {
+        document.getElementById("showupm").style.display = "block";
+        document.getElementById("crossm").classList.toggle("rotatedm");
+        m++
+    }
+    else {
+        document.getElementById("showupm").style.display = "none";
+        document.getElementById("crossm").classList.toggle("rotatedm");
+        m--;
+    }
+}
+
+var n = 0;
+function shown() {
+
+    if (n == 0) {
+        document.getElementById("showupn").style.display = "block";
+        document.getElementById("crossn").classList.toggle("rotatedn");
+        n++;
+    }
+    else {
+        document.getElementById("showupn").style.display = "none";
+        document.getElementById("crossn").classList.toggle("rotatedn");
+        n--;
+    }
+}
